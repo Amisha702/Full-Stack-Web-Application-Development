9d865755c2d9fd26a72f422458cc6940f2e4b573
Amisha702 <chaudharyamisha879@gmail.com>
2026-08-18 21:50:49 +0545

    Changes in config after deployment failure

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  nitro: {
+    preset: "node-server",
+  },
+
   tanstackStart: {
     server: {
       entry: "server",
