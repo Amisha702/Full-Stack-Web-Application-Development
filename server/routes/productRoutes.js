ba3938795ed08cb2bca92e20a92e6807ad069607
Amisha702 <chaudharyamisha879@gmail.com>
2026-08-18 16:56:08 +0545

    Added routes to the sever

---
+++ server/routes/productRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth');
+const upload = require('../middleware/upload');
+const ctrl = require('../controllers/productController');
+
+router.get('/', auth, ctrl.getAll);
+router.get('/:id', auth, ctrl.getOne);
+
+router.post('/', auth, upload.single('image'), ctrl.create);
+router.put('/:id', auth, upload.single('image'), ctrl.update);
+router.delete('/:id', auth, ctrl.remove);
+
+module.exports = router;
---
+++ server/routes/supplierRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+const ctrl = require('../controllers/supplierController');
+
+// All supplier routes require authentication
+router.get('/', auth, ctrl.getAll);
+router.get('/:id', auth, ctrl.getOne);
+
+router.post('/', auth, ctrl.create);
+router.put('/:id', auth, ctrl.update);
+router.delete('/:id', auth, ctrl.remove);
+
+module.exports = router;
